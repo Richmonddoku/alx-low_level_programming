531a89bc9cd637488850a26724493e9e289b11a1
Richmond <richieanderson370@gmail.com>
2023-02-21 07:06:50 -0800

    Tasks-c-Low_level_progamming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Description - prits the alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
